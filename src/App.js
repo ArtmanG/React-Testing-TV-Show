63a4582bc200d568cea1fd125367084d261e70e5
Gregory Hawman <g.hawman@gmail.com>
2020-03-23 14:24:16 -0700

    seperated the api call and got the useEffect to still work and grab that data.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
+import { fetchShow } from './api/fetchshow';
 import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
@@ -15,17 +16,11 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
+    fetchShow()
         .then(res => {
           setShow(res.data);
           setSeasons(formatSeasons(res.data._embedded.episodes));
         });
-    };
-    fetchShow();
   }, []);
 
   const handleSelect = e => {
